db7777aa8118ce67e93fac7eadd87b12af982e2f
gladandres <47525456+gladandres@users.noreply.github.com>
2020-01-29 17:33:07 +0300

    Create article_class.py

---
+++ article_class.py
@@ -0,0 +1,7 @@
+class Article(object):
+
+    def __init__(self, name, link, header, text):
+        self.name = name
+        self.link = link
+        self.header = header
+        self.text = text
