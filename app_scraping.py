164581a3d98d010c178cb1b6560a3140764bfee9
gladandres <47525456+gladandres@users.noreply.github.com>
2020-02-05 08:47:05 +0300

    Update app_scraping.py

--- app_scraping.py
+++ app_scraping.py
@@ -5,7 +5,9 @@ from article_class import *
 def get_list_news(list_object):
     list_news = []
     for article in list_object:
-        news = {'news':article.name,'link':article.link,'header':article.header, 'text':article.text}
+        news = {
+            'news':article.name,'link':article.link,
+            'header':article.header, 'text':article.text}
         list_news.append(news)
     return list_news
 
